896693127b8725306e663b6c694198b511d2fc68
Fokam Manuel <arnolfokam23@gmail.com>
2021-09-25 15:51:56 +0200

    add computation graph

--- agents/segmentation/segmentation_agent.py
+++ agents/segmentation/segmentation_agent.py
@@ -5,6 +5,8 @@ from agents.handlers.state_handlers import StateHandler
 from agents.handlers.summary_handler import SummaryHandler
 from agents.segmentation.utils.get_model import get_model
 
+# Thank to https://github.com/JiajieMo/OctaveUNet/blob/master/src/agents/retina/retinal_vessel.py
+
 
 class SegmentationAgent(SummaryHandler, StateHandler, OutputHandler, ABC):
     def __init__(self, external_configs_list: list = None):
@@ -15,7 +17,13 @@ class SegmentationAgent(SummaryHandler, StateHandler, OutputHandler, ABC):
         self.handle_paths()
 
         # get sample keys and data loaders
-        self.handle_data()
+        # self.handle_data()
+
+        # get device and setup random seed and benchmark
+        self.handle_device()
+
+        # get model, criterion, optimizer, and lr_scheduler
+        self.handle_computation_graph()
 
     def get_model(self):
         """Get model."""
